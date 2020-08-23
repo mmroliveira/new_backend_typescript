c20310ee71adb9782a885b53dd0862be0635c7a4
Murilo G. de Oliveira <murilo_879@hotmail.com>
2020-08-23 00:26:05 -0400

    Add Eslint

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-	return response.json({ message: 'Hellow Word' });
+  response.json({ message: 'Hellow Word' });
 });
 
 app.listen(3333);
- 
