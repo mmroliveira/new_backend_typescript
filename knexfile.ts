2c8e05fda4a2aefbf7a5d1d660b81949a4e901be
Murilo G. de Oliveira <murilo_879@hotmail.com>
2020-09-06 23:07:13 -0400

    add prettier

---
+++ knexfile.ts
@@ -0,0 +1,48 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'Projeto',
+      user: 'postgres',
+      password: '102030',
+    },
+    migrations: {
+      directory: './src/database/migrations',
+    },
+    useNullAsDefault: true,
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
